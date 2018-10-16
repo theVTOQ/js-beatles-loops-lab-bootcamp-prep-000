b2690bdbf7722c38f8acc52864d782596b951b8f
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-16 00:54:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = []
+  for(var i = 0; i < musicians.length; i++){
+    emptyArray.push(musicians[i] + "plays " + instruments[i])
+  }
+}
