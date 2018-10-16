54ba8d8c94ecd446ae4f116d1ff254eeccc4ed42
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-16 01:06:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ function theBeatlesPlay(musicians, instruments){
   for(var i = 0; i < musicians.length; i++){
     emptyArray.push(musicians[i] + "plays " + instruments[i])
   }
+  
+  return emptyArray
 }
 
 function johnLennonFacts(facts){
+  var i = 0;
+  while(i < facts.length){
+    facts[i] = facts[i] + "!!!";
+    i++;
+  }
   
+  return facts
+}
+
+function iLoveTheBeatles(n){
+  var emptyArray = []
+  do {
+    emptyArray.push("I love the Beatles!")
+    n++;
+  } while(n < 15)
 }
